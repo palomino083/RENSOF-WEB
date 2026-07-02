392492dfdb0e2736a2fa93191bf0235645fd827e
palomino083 <palomino083@hotmail.com>
2026-07-01 19:10:06 -0500

    Descripción de los cambios

--- app/core/config.py
+++ app/core/config.py
@@ -5,9 +5,9 @@ from typing import Final
 BASE_DIR: Final = Path(__file__).resolve().parents[2]
 TEMPLATES_DIR: Final = BASE_DIR / "templates"
 ASSETS_DIR: Final = BASE_DIR / "assets"
-ALVENT_FRONTEND_ORIGIN: Final = os.getenv("ALVENT_FRONTEND_ORIGIN", "http://127.0.0.1:3100")
+ALVENT_FRONTEND_ORIGIN: Final = os.getenv("ALVENT_FRONTEND_ORIGIN", "https://alvent-frontend.onrender.com")
 ALVENT_FRONTEND_BASE_PATH: Final = os.getenv("ALVENT_FRONTEND_BASE_PATH", "/alven/app")
-ALVENT_BACKEND_ORIGIN: Final = os.getenv("ALVENT_BACKEND_ORIGIN", "http://127.0.0.1:8100")
+ALVENT_BACKEND_ORIGIN: Final = os.getenv("ALVENT_BACKEND_ORIGIN", "https://alvent-backend.onrender.com")
 ALVENT_APP_URL: Final = os.getenv("ALVENT_APP_URL", "/alven/app/login")
 
 PAGES: Final = {
